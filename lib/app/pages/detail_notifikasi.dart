e2039c5f82576f2c96d09ccebc45b8b2c6d3edd0
Airyyy23 <wayangseno123@gmail.com>
2024-05-10 17:10:24 +0700

    feat: [WS] revisi notifikasi page

--- lib/app/pages/detail_notifikasi.dart
+++ lib/app/pages/detail_notifikasi.dart
@@ -1,4 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        'Notifikasi',
+        style: TextStyle(fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+}
+
+class CustomBody extends StatelessWidget {
+  final Map<String, dynamic>? notification;
+
+  const CustomBody({Key? key, required this.notification}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => notification != null
+        ? SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          notification!['icon'],
+                          width: 80,
+                          height: 80,
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          notification!['keterangan'],
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF979090)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        notification!['judul'],
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        notification!['waktu'],
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF979090),
+                            fontSize: 12),
+                      ),
+                      SizedBox(height: 8),
+                      Divider(
+                        color: Colors.grey,
+                      ),
+                      SizedBox(height: 8),
+                      Text(notification!['deskripsi']),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        : Center(
+            child: Text('Tidak ada notifikasi'),
+          ));
+  }
+}
 
 class DetailNotifikasi extends StatelessWidget {
   DetailNotifikasi({Key? key}) : super(key: key);
@@ -12,7 +95,7 @@ class DetailNotifikasi extends StatelessWidget {
       'deskripsi':
           'Laporan anda tentang  “tawuran antar sekolah di depan rumah arya” telah dikirim'
     },
-  ];
+  ].obs;
 
   @override
   Widget build(BuildContext context) {
@@ -20,69 +103,8 @@ class DetailNotifikasi extends StatelessWidget {
         notificationsList.isNotEmpty ? notificationsList.first : null;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Notifikasi',
-          style: TextStyle(fontWeight: FontWeight.w600),
-        ),
-      ),
-      body: notification != null
-          ? SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            notification['icon'],
-                            width: 80,
-                            height: 80,
-                          ),
-                          SizedBox(height: 16),
-                          Text(
-                            notification['keterangan'],
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xFF979090)),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 40),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          notification['judul'],
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          notification['waktu'],
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xFF979090),
-                              fontSize: 12),
-                        ),
-                        SizedBox(height: 8),
-                        Divider(
-                          color: Colors.grey,
-                        ),
-                        SizedBox(height: 8),
-                        Text(notification['deskripsi']),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          : Center(
-              child: Text('Tidak ada notifikasi'),
-            ),
+      appBar: CustomAppBar(),
+      body: CustomBody(notification: notification),
     );
   }
 }
--- lib/app/pages/notifikasi.dart
+++ lib/app/pages/notifikasi.dart
@@ -1,7 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:lapor_kasat/app/pages/detail_notifikasi.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        'Notifikasi',
+        style: TextStyle(fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+}
+
+class CustomBody extends StatelessWidget {
+  final List<Map<String, dynamic>> notificationsList;
+
+  const CustomBody({Key? key, required this.notificationsList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => ListView.builder(
+          itemCount: notificationsList.length,
+          itemBuilder: (BuildContext context, int index) {
+            final notification = notificationsList[index];
+            return Card(
+              elevation: 4, // set the elevation (shadow)
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16), // rounded corners
+              ),
+              color: Colors.white,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16), // rounded corners
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(16.0),
+                  leading: Image.asset(notification['icon']),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            notification['keterangan'],
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                                color: Color(0xFF979090)),
+                          ),
+                          Text(
+                            notification['waktu'],
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                                color: Color(0xFF979090)),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        notification['judul'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(notification['deskripsi']),
+                    ],
+                  ),
+                  onTap: () {
+                    Get.to(DetailNotifikasi());
+                  },
+                ),
+              ),
+            );
+          },
+        ));
+  }
+}
 
 class Notifikasi extends StatelessWidget {
   Notifikasi({Key? key}) : super(key: key);
@@ -33,70 +116,8 @@ class Notifikasi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Notifikasi',
-          style: TextStyle(fontWeight: FontWeight.w600),
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: notificationsList.length,
-        itemBuilder: (BuildContext context, int index) {
-          final notification = notificationsList[index];
-          return Card(
-            elevation: 4, // set the elevation (shadow)
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16), // rounded corners
-            ),
-            color: Colors.white,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16), // rounded corners
-              child: ListTile(
-                contentPadding: EdgeInsets.all(16.0),
-                leading: Image.asset(notification['icon']),
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          notification['keterangan'],
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 12,
-                              color: Color(0xFF979090)),
-                        ),
-                        Text(
-                          notification['waktu'],
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 12,
-                              color: Color(0xFF979090)),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      notification['judul'],
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    SizedBox(height: 4),
-                    Text(notification['deskripsi']),
-                  ],
-                ),
-                onTap: () {
-                  Get.to(DetailNotifikasi());
-                },
-              ),
-            ),
-          );
-        },
-      ),
+      appBar: CustomAppBar(),
+      body: CustomBody(notificationsList: notificationsList),
     );
   }
 }
